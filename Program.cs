499d58beca3aa5466d09afa7656bf1577d1d5dca
Celio Iyama <celio.iyama@gmail.com>
2021-09-24 15:58:51 -0300

    Exercicios de fixaçao de concatenaçao de dados

--- Program.cs
+++ Program.cs
@@ -7,13 +7,32 @@ namespace Projeto1
     {
         static void Main(string[] args)
         {
-            int idade = 42;
-            double saldo = 10.35784;
-            string nome = "Carlos";
+            string produto1 = "computador";
+            string produto2 = "Mesa de escritório";
 
-            Console.WriteLine("{0} tem {1} anos e tem saldo igual a R${2:F2} reais", nome, idade, saldo); /* Placeholder formatado com tipos double com 2 casas decimais */           
-            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a R${saldo:F2} reais"); /* interpolação formatado com tipos double com 2 casas decimais */           
-            System.Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a R$" + saldo.ToString("F2") + " reais");
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto1}, cujo preço é R$ {preco1:F2}");
+            Console.WriteLine($"{produto2}, cujo preço é R$ {preco2:F2}");
+            System.Console.WriteLine("");
+            System.Console.WriteLine("");
+
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+            System.Console.WriteLine("");
+            System.Console.WriteLine("");
+
+            Console.WriteLine("Medida com oito casas decimais: {0:F8}", medida);
+            Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
+            Console.WriteLine("Separador decimal invariant culture: " + (medida.ToString("F3", CultureInfo.InvariantCulture)));
+            
+            
         }
     }
 }
