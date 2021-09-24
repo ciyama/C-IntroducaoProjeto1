17f438dc2f9ab49a3f62ca8fb1d7630f9180c8ad
Celio Iyama <celio.iyama@gmail.com>
2021-09-24 11:11:00 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola mundo!");
+            Console.WriteLine("Bom dia!");
+
+            Console.ReadLine();
+        }
+    }
+}
