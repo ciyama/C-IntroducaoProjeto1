6fe05c6bea8f58d00c3aee2a5da36d6b1c3a53fe
Celio Iyama <celio.iyama@gmail.com>
2021-09-26 18:42:41 -0300

    Operadores aritmeticos

--- Program.cs
+++ Program.cs
@@ -8,43 +8,39 @@ namespace Projeto1
         static void Main(string[] args)
         {
             
-            float a;
-            double b;
-           
-            a = 5.1f;
-            b = a;
+            int resultado1 = 3 + 4 * 2;
+            int resultado2 = (3 + 4) * 2;
+            int resultado3 = 17 % 3;
+            double resultado4 = 10 / 8 ;
+            double resultado5 = (double) 10 / 8;
+            double resultado6 = 10.0 / 8;
+            double resultado7 = 10.0 / 8.0;
 
-            System.Console.WriteLine(a);
-            System.Console.WriteLine(b);
+            // Bascara x = (-b +- raiz quadrada de b ao quadrado - 4 a. c ) / 2.a
+            double a = 1.0, b = -3.0, c = -4.0;
 
-            double c;
-            float d;
+            // double delta = b * b - 4 * a * c;
+            double delta = Math.Pow(b, 2) - 4 * a * c;
 
-            c = 5.1;
-            d = (float)c;  /* Casting para tranformação forçada de um Double para um Float de 8 byte para 4 byte.*/
-
-            System.Console.WriteLine(c);
-            System.Console.WriteLine(d);
-
-            int f;
-            f = (int)c;
-
-            System.Console.WriteLine(f);
-
-            int g;
-            int h;
-
-            g = 5;
-            h = 2;
-            double resultado = (double) g / h; /* Cuidado com o resultado pois quando estamos dividindo 2 numeros inteiros o resultado é inteiro, então devemos fazer um casting para o resultado ficar correto */
-
-            System.Console.WriteLine(resultado);     
+            double bhaskara1 = (-b + Math.Sqrt(delta)) / (2 * a) ;
+            double bhaskara2 = (-b - Math.Sqrt(delta)) / (2 * a) ;
 
 
 
 
 
-            
+
+
+            System.Console.WriteLine("Resultado 1: " + resultado1);
+            System.Console.WriteLine("Resultado 2: " + resultado2);
+            System.Console.WriteLine("Resultado 3: " + resultado3);
+            System.Console.WriteLine("Resultado 4: " + resultado4);
+            System.Console.WriteLine("Resultado 5: " + resultado5);
+            System.Console.WriteLine("Resultado 6: " + resultado6);
+            System.Console.WriteLine("Resultado 7: " + resultado7);
+            System.Console.WriteLine("Resultado delta: " + delta);
+            System.Console.WriteLine("Resultado bhaskara1= " + bhaskara1);
+            System.Console.WriteLine("Resultado bhaskara2= " + bhaskara2);
         }
     }
 }
